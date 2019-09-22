58b88e225fcf75656b133de93d80acfd326df7f1
Maksim <familinov@gmail.com>
2019-09-22 15:44:26 +0500

    доделал пины на карте

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ var checkouts = ['12:00', '13:00', '14:00'];
 var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var AD_TITLE = 'Предложение №';
 var LOCATION_X_MIN = 100;
-var LOCATION_X_MAX = 600;
+var LOCATION_X_MAX = 1000;
 var LOCATION_Y_MIN = 130;
 var LOCATION_Y_MAX = 630;
 var MIN_PRICE = 5000;
@@ -16,7 +16,6 @@ var MIN_ROOMS = 1;
 var MAX_ROOMS = 3;
 var MIN_GUESTS = 1;
 var MAX_GUESTS = 3;
-
 
 var getRandomInterval = function(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
@@ -39,16 +38,16 @@ var getRandomArrayFromArray = function(array) {
   return randomArray;
 }
 
-var createAd = function(count) {
+var createAds = function(count) {
   var ads = [];
   for (var i = 0; i < count; i ++) {
-    var xCoordinate = getRandomInterval(LOCATION_X_MIN, LOCATION_Y_MAX);
-    var yCoordinate = getRandomInterval(LOCATION_Y_MAX, LOCATION_Y_MAX);
-    var ad = {
+    var xCoordinate = getRandomInterval(LOCATION_X_MIN, LOCATION_X_MAX);
+    var yCoordinate = getRandomInterval(LOCATION_Y_MIN, LOCATION_Y_MAX);
+    var adElement = {
       'author': {
-        'avatar': 'img/avatars/user' + i + '.png'
+        'avatar': 'img/avatars/user0' + (i + 1) + '.png'
       },
-      'locataion': {
+      'location': {
         x: xCoordinate,
         y: yCoordinate
       },
@@ -66,10 +65,40 @@ var createAd = function(count) {
         'photos': getRandomArrayFromArray(photos)
       }
     };
-    ads.push(ad);
+    ads.push(adElement);
   }
   return ads;
 }
 
-console.log(createAd(8));
+var ads = createAds(8);
+
+console.log(ads);
+
+var map = document.querySelector('.map');
+
+map.classList.remove('map--faded');
+
+var similarPin = document.querySelector('#pin').content.querySelector('.map__pin');
+var similarPinList = document.querySelector('.map__pins');
+
+var renderAd = function(ad) {
+  var pinElement = similarPin.cloneNode(true);
+
+  pinElement.setAttribute('style', 'left:' + ad.location.x + 'px' + '; top:' + ad.location.y + 'px');
+  pinElement.setAttribute('alt', ad.offer.title);
+  pinElement.querySelector('img').setAttribute('src', ad.author.avatar);
+
+  return pinElement;
+}
+
+console.log(renderAd(ads[2]));
+console.log(renderAd(ads[3]));
+console.log(renderAd(ads[4]));
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < ads.length; i++) {
+  fragment.appendChild(renderAd(ads[i]));
+}
+map.appendChild(fragment);
+
 
