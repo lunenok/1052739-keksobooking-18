9f34ffe456c88b8552daf8f5b9b571f4c2be2854
Maksim <familinov@gmail.com>
2019-11-10 12:23:08 +0500

    Добавил фильтрацию по цене

--- js/filter.js
+++ js/filter.js
@@ -4,26 +4,51 @@
   var roomsCount = document.querySelector('#housing-rooms');
   var guestsCount = document.querySelector('#housing-guests');
   var roomType = document.querySelector('#housing-type');
+  var priceValueInput = document.querySelector('#housing-price');
   var MAX_PIN_COUNT = 5;
   var DEFAULT_FILTER_VALUE = 'any';
+  var PRICE_INTERVALS = {
+    any: 'any',
+    low: {
+      min: 0,
+      max: 9999
+    },
+    middle: {
+      min: 10000,
+      max: 49999
+    },
+    high: {
+      min: 50000,
+      max: Infinity
+    }
+  };
 
-  var filterType = function (item) {
+  var typeFilter = function (item) {
     var selectedType = roomType.options[roomType.selectedIndex].value;
     return selectedType === DEFAULT_FILTER_VALUE ? true : item.offer.type === selectedType;
   };
 
-  var filterByRooms = function (item) {
+  var priceFilter = function (item) {
+    var selectedPrice = priceValueInput.options[priceValueInput.selectedIndex].value;
+    var priceValue = item.offer.price;
+    return selectedPrice === PRICE_INTERVALS.any
+      ? true
+      : priceValue >= PRICE_INTERVALS[selectedPrice].min &&
+        priceValue <= PRICE_INTERVALS[selectedPrice].max;
+  };
+
+  var roomsFilter = function (item) {
     return roomsCount.value === DEFAULT_FILTER_VALUE ? true : item.offer.rooms === parseInt(roomsCount.value, 10);
   };
 
-  var filterByGuests = function (item) {
+  var guestsFilter = function (item) {
     return guestsCount.value === DEFAULT_FILTER_VALUE ? true : item.offer.guests === parseInt(guestsCount.value, 10);
   };
 
   var filterData = function (data) {
     return data
       .filter(function (item) {
-        return filterType(item) && filterByRooms(item) && filterByGuests(item);
+        return typeFilter(item) && roomsFilter(item) && guestsFilter(item) && priceFilter(item);
       })
       .slice(0, MAX_PIN_COUNT);
   };
